3f77e65ef2f15da690d09db404fea2cdce7ea7bf
JULIANEMI <ortizrivero3108@gmail.com>
2026-05-16 13:06:43 -0600

    Optimización

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_compress import Compress
 
 """
 ==========================================================================
@@ -7,6 +8,20 @@ CONFIGURACIÓN Y CREACIÓN DEL OBJETO APP
 """
 def create_app():
     app = Flask(__name__)
+
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
+
+    app.config['COMPRESS_MIN_SIZE'] = 0
+    app.config['COMPRESS_MIMETYPES'] = [
+        'text/html', 
+        'text/css', 
+        'text/xml', 
+        'application/json', 
+        'application/javascript', 
+        'image/svg+xml'
+    ]
+
+    Compress(app)
     
     with app.app_context():
         from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, jsonify
+import os
+from flask import render_template, request, jsonify, send_from_directory
 from flask import current_app as app
-from .utils import agregar_apartado_a_json
-from .utils import eliminar_apartado_de_json
+from .utils import agregar_apartado_a_json, eliminar_apartado_de_json
 
 """
 ==========================================================================
@@ -11,6 +11,18 @@ DEFINICIÓN DE RUTAS Y LÓGICA DE VISTAS
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', 
+        mimetype='image/vnd.microsoft.icon'
+    )
+
+@app.route('/robots.txt')
+def robots():
+    return "User-agent: *\nDisallow:", 200, {'Content-Type': 'text/plain'}
 
 """
 ==========================================================================
@@ -35,7 +47,7 @@ def guardar_apartado():
 @app.route('/api/config/apartados/eliminar', methods=['POST'])
 def borrar_apartado():
     datos = request.get_json()
-    nombre = datos.get('nombre_apartado') # Recibimos el nombre del front
+    nombre = datos.get('nombre_apartado')
     
     try:
         eliminar_apartado_de_json(nombre)
